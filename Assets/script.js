6d43d337d0da3d5e0a0e212ea2676cc7d2dcedf9
Will <wmalos22@gmail.com>
2021-03-02 11:00:03 -0800

    added first weather api and html

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+var apiKey = "52cd24779947814501b070ebef234141"
+var search = document.getElementById ("searchCity")
+
+
+search.addEventListener("click", function() {
+    var input = document.getElementById ("input").value;
+    console.log(input);
+    currentWeather(input)
+})
+
+function currentWeather (city) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&appid="+ apiKey+ "&units=imperial"
+    console.log(url)
+    fetch(url)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (response) {
+            console.log(response);
+            var h1 = document.createElement("h1");
+            h1.textContent = response.name;
+            document.getElementById ("currentInfo").append(h1);
+        });
+}
+
+
