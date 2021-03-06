fc2f74c97686ec23949f7595ee6a5fd33f57c57f
Will <wmalos22@gmail.com>
2021-03-05 21:15:05 -0800

    formatting

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,15 @@
 var apiKey = "52cd24779947814501b070ebef234141"
-var search = document.getElementById ("searchCity")
+var search = document.getElementById("searchCity")
 
 
-search.addEventListener("click", function() {
-    var input = document.getElementById ("input").value;
+search.addEventListener("click", function () {
+    var input = document.getElementById("input").value;
     console.log(input);
     currentWeather(input)
 })
 
-function currentWeather (city) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&appid="+ apiKey+ "&units=imperial"
+function currentWeather(city) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial"
     console.log(url)
     fetch(url)
         .then(function (data) {
@@ -19,7 +19,7 @@ function currentWeather (city) {
             console.log(response);
             var h2 = document.createElement("h2");
             h2.textContent = response.name;
-            document.getElementById ("currentInfo").append(h2);
+            document.getElementById("currentInfo").append(h2);
         });
 }
 
