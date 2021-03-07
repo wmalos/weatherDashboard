0a28bbc15012e524fe22826f66486032ca67f2d3
Will <wmalos22@gmail.com>
2021-03-06 22:20:16 -0800

    editing forecast function

--- Assets/script.js
+++ Assets/script.js
@@ -79,36 +79,4 @@ function fivedayForecast(city) {
         document.getElementById("forecast").append(h6);
     })
 
-    $.ajax({
-        url
-      }).then(function (response){
-
-    console.log(response)
-    console.log(response.dt)
-    $('#forecast').empty();
-
-
-    let results = response.list;
-    console.log(results)
-
-    for (let i = 0; i < results.length; i++) {
-
-        let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
-        let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
-        console.log(day);
-        console.log(hour);
-
-        if(results[i].dt_txt.indexOf("12:00:00") !== -1){
-
-            const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
-            const cardBody = $("<div>").addClass("card-body p-3 forecast")
-            const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
-            const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + " °F");
-            const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + data[i].main.humidity + "%");
-        
-        cardBody.append(cityDate, image, temperature, humidity);
-        card.append(cardBody);
-        $("#forecast").append(card);
-        }
-    }
 }
